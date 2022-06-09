bd66b05ed937d3cc37fdbe5ee874ce59cb2608aa
KhalezAlex <Klozevitz@yandex.ru>
2022-06-09 11:37:14 +0300

    IP validation added

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,17 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void setIp(int[] ipI, String ipS) {
+    private static void setIp(int[] ipI, String ipS) {
         String[] ipStr = ipS.split("\\.");
         for (int i = 0; i < 4; i++)
             ipI[i] = Integer.parseInt(ipStr[i]);
+    }
+
+    private static boolean isValid(int[] ipI) {
+        for (int j : ipI)
+            if (!(j > -1 && j < 256))
+                return false;
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
@@ -19,20 +26,22 @@ public class Main {
         byte[][][][] uniqueArray = new byte[256][256][256][256];        //массив 0/1, где 0- ip еще не встречался, а 1- встречался
         BufferedReader bR = new BufferedReader(new FileReader(path));
 
-        long t = System.currentTimeMillis();
+        //long t = System.currentTimeMillis();
         while(true) {
             if ((str = bR.readLine()) == null) {
                 break;
             }
             else {
                 setIp(ip, str);
-                if (uniqueArray[ip[0]][ip[1]][ip[2]][ip[3]] == 0) {     //если поступивший в обработку ip уникаклен (в массиве - 0)
-                    uniqueArray[ip[0]][ip[1]][ip[2]][ip[3]] = 1;        //"стреляем" в массив единичкой и добавляемк счетчику единицу
-                    unique ++;
+                if (isValid(ip)) {
+                    if (uniqueArray[ip[0]][ip[1]][ip[2]][ip[3]] == 0) {     //если поступивший в обработку ip уникаклен (в массиве - 0)
+                        uniqueArray[ip[0]][ip[1]][ip[2]][ip[3]] = 1;        //"стреляем" в массив единичкой и добавляемк счетчику единицу
+                        unique++;
+                    }
                 }
             }
         }
         System.out.println(unique);
-        System.out.println(System.currentTimeMillis() - t);
+        //System.out.println(System.currentTimeMillis() - t);
     }
 }
